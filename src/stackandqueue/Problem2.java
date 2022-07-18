30e4044eb845c945dfd136ac43389ee5551f35ec
pandaer <liwenhaolx@gmail.com>
2022-07-18 10:22:51 +0800

    两两交换链表中的节点 递归版

--- src/stackandqueue/Problem2.java
+++ src/stackandqueue/Problem2.java
@@ -6,18 +6,11 @@ package stackandqueue;
 @SuppressWarnings("all")
 public class Problem2 {
     public ListNode swapPairs(ListNode head) {
+        //递归
         if (head == null || head.next == null) return head;
-        ListNode first = head;
         ListNode second = head.next;
-        head = second;
-
-        while(first != null && second != null) {
-            ListNode temp = second.next;
-            second.next = first;
-            first.next = temp != null && temp.next != null ? temp.next : temp;
-            second  = temp != null && temp.next != null ? temp.next : null;
-            first = temp != null ? temp : null;
-        }
-        return head;
+        head.next = swapPairs(head.next.next);
+        second.next = head;
+        return second;
     }
 }
