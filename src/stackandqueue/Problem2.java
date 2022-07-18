2881e08ba3c16da87c21adcd5be01039cbc1cbdb
pandaer <liwenhaolx@gmail.com>
2022-07-18 10:48:08 +0800

    栈的思想实现

--- src/stackandqueue/Problem2.java
+++ src/stackandqueue/Problem2.java
@@ -1,5 +1,7 @@
 package stackandqueue;
 
+import java.util.LinkedList;
+
 /**
  * https://leetcode.cn/problems/swap-nodes-in-pairs/
  */
@@ -7,17 +9,21 @@ package stackandqueue;
 public class Problem2 {
     public ListNode swapPairs(ListNode head) {
         if (head == null || head.next == null) return head;
-
+        //用栈的思想实现
+        LinkedList<ListNode> stack = new LinkedList<>();
         ListNode cur = head;
-        ListNode newHead = head.next;
+        ListNode temp = new ListNode(-1);
+        head = temp;
         while(cur != null && cur.next != null) {
-            ListNode temp = cur;
-            cur = cur.next;
-            temp.next = cur.next;
-            cur.next = temp;
-            cur = temp.next;
-            if (cur != null && cur.next != null) temp.next = cur.next;
+            for (int i =0; i<2; i++,cur = cur.next){
+                stack.push(cur);
+            }
+            for (int i = 0; i<2;i++) {
+                temp.next = stack.pop();
+                temp = temp.next;
+            }
         }
-        return newHead;
+        temp.next = cur;
+        return head.next;
     }
 }
