60fae191b1503e6ff960d4ed19974e50b195b7fa
pandaer <liwenhaolx@gmail.com>
2022-07-18 20:38:22 +0800

    K 个一组翻转链表 递归办法

---
+++ src/stackandqueue/ReverseNodesInKGroup.java
@@ -0,0 +1,47 @@
+package stackandqueue;
+
+public class ReverseNodesInKGroup {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode cur = head;
+        for (int i = 0; cur != null && i<k-1; i++) {
+            cur = cur.next;
+        }
+        if (cur == null) return head;
+        ListNode next = cur.next;
+        reverse(head,cur);
+        head.next = reverseKGroup(next,k);
+
+        return cur;
+
+    }
+    public void reverse(ListNode start, ListNode tail) {
+        if (start == null || start.next == null) return;
+        ListNode prev = null;
+        ListNode cur = start;
+        ListNode last = tail.next;
+        while(cur != last && cur != null) {
+            ListNode temp = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = temp;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        ReverseNodesInKGroup r = new ReverseNodesInKGroup();
+        ListNode head = r.init();
+        r.reverseKGroup(head,2);
+
+    }
+
+    public ListNode init(){
+        ListNode prev = new ListNode(-1);
+        ListNode temp = prev;
+        for (int i = 0; i<5;i++) {
+            temp.next = new ListNode(i+1);
+            temp = temp.next;
+        }
+        return prev.next;
+    }
+}
